1490f8c7d024e74824bfe7aead0057911300a318
ayel-mou <ayel-mou@c3r10p3.1337.ma>
2024-12-16 10:58:28 +0100

    update

--- vectors.c
+++ vectors.c
@@ -35,3 +35,15 @@ float dot_product(t_vec3 v1, t_vec3 v2)
 {
 	return ((v1.x * v2.x) + (v1.y * v2.y) +(v1.z * v2.z));
 }
+
+float vector_magnitude(t_vec3 vector)
+{
+    return (sqrt((vector.x * vector.x) + (vector.y * vector.y) + (vector.z * vector.z)));
+}
+
+t_vec3 vector_normalize(t_vec3 vector)
+{
+	t_vec3 vec;
+
+	
+}
--- vectors.h
+++ vectors.h
@@ -12,3 +12,5 @@ t_vec3 new_vector(float x, float y, float z);
 t_vec3 add_vector(t_vec3 v1, t_vec3 v2);
 t_vec3 subtract_vector(t_vec3 v1, t_vec3 v2);
 float dot_product(t_vec3 v1, t_vec3 v2);
+float vector_magnitude(t_vec3 vector);
+t_vec3 vector_normalize(t_vec3 vector);
